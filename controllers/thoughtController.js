7921885c5ebb1af9d2977418f423669414701e02
Max McDonough <max.mcdonough@gmail.com>
2022-09-08 21:40:35 -0400

    User and thought controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,31 @@
+import { Thought, Reaction } from '../models/index.js';
+
+const getAllThoughts = async (req, res) => {
+
+};
+
+const createNewThought = async (req, res) => {
+
+};
+
+const getOneThought = async (req, res) => {
+
+};
+
+const updateOneThought = async (req, res) => {
+
+};
+
+const removeOneThought = async (req, res) => {
+
+};
+
+const createReaction = async (req, res) => {
+
+};
+
+const removeReaction = async (req, res) => {
+
+};
+
+export {getAllThoughts, createNewThought, getOneThought, updateOneThought, removeOneThought, createReaction, removeReaction};
---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+import { User, Thought } from '../models/index.js';
+
+const getAllUsers = async (req, res) => {
+
+};
+
+const getOneUser = async (req, res) => {
+
+};
+
+const createOneUser = async (req, res) => {
+
+};
+
+const updateOneUser = async (req, res) => {
+
+};
+
+const removeOneUser = async (req, res) => {
+
+};
+
+const addFriend = async (req, res) => {
+
+};
+
+const removeFriend = async (req, res) => {
+
+};
+
+export {getAllUsers, getOneUser, createOneUser, updateOneUser, removeOneUser, addFriend, removeFriend};

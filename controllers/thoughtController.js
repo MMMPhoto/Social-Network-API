d24929f65ed0bf7d72fad8810298f2615872e83a
Max McDonough <max.mcdonough@gmail.com>
2022-09-08 23:40:07 -0400

    added some thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,15 +1,36 @@
 import { Thought, Reaction } from '../models/index.js';
 
 const getAllThoughts = async (req, res) => {
-
+    try {
+        const allThoughts = await Thought.find();
+        console.log(allThoughts);
+        res.status(200).json(allThoughts);    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
 };
 
 const createNewThought = async (req, res) => {
-
+    try {
+        const newThought = await Thought.create(req.body);
+        res.status(200).json(newThought);    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
 };
 
 const getOneThought = async (req, res) => {
-
+    try {
+        const oneThought = await Thought.findOne({ _id: req.params.thoughtId })
+            .populate('username');
+        console.log(oneThought);
+        !oneThought ? res.status(404).json({ message: 'No user with that ID' }) : res.status(200).json(oneThought);    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
 };
 
 const updateOneThought = async (req, res) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,36 +3,31 @@ import { User, Thought } from '../models/index.js';
 const getAllUsers = async (req, res) => {
     try {
         const allUsers = await User.find();
-        console.log(allUsers);
         res.status(200).json(allUsers);    
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
-    }
+    };
 };
 
 const getOneUser = async (req, res) => {
     try {
         const oneUser = await User.findOne({ _id: req.params.userId });
-        console.log(oneUser);
         !oneUser ? res.status(404).json({ message: 'No user with that ID' }) : res.status(200).json(oneUser);    
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
-    }
-
+    };
 };
 
 const createOneUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
-        console.log(newUser);
         res.status(200).json(newUser);    
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
-    }
-
+    };
 };
 
 const updateOneUser = async (req, res) => {

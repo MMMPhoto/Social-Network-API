e7305233011b0b788aa9f1a5dfcacf2730de7753
Max McDonough <max.mcdonough@gmail.com>
2022-09-14 10:40:06 -0400

    updated timestamp formatting

--- models/Thought.js
+++ models/Thought.js
@@ -1,14 +1,25 @@
 import mongoose from "mongoose";
 const { Schema, model, Types } = mongoose;
 
+
+
+const prettyTimestamp = (dateTime) => {
+    const date = dateTime.toDateString();
+    const time = dateTime.toLocaleTimeString();
+    const prettyDateTime = `${date} at ${time}`;
+    return prettyDateTime;
+};
+
+
 const reactionSchema = new Schema(
     {
         reactionId: {type: Schema.Types.ObjectId, default: () => new Types.ObjectId()},
         reactionBody: {type: String, required: true, maxLength: 280},
         username: {type: String, required: true},
         createdAt: {type: Date, default: Date.now,
-            // get: (date) => timeSince(date)
-        }
+            get: (dateTime) => prettyTimestamp(dateTime)
+        },
+        updatedAt: {type: Date, get: (dateTime) => prettyTimestamp(dateTime)},
     },
     {
         timestamps: true,
@@ -22,11 +33,12 @@ const reactionSchema = new Schema(
 const thoughtSchema = new Schema(
     {
         thoughtText: {type: String, required: true, maxLength: 280},
-        createdAt: {type: Date, default: Date.now, 
-            // get: (date) => timeSince(date)
+        createdAt: {type: Date, default: Date.now,
+            get: (dateTime) => prettyTimestamp(dateTime)
         },
         username: {type: String, required: true},
-        reactions: [reactionSchema]
+        reactions: [reactionSchema],
+        updatedAt: {type: Date, get: (dateTime) => prettyTimestamp(dateTime)}
     },
     {
         timestamps: true,
